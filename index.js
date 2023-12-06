c1c5ae64cb92336e477caac1a46b8fe96cf286f7
Oleksii <yastrebov.oleksii@gmail.com>
2023-12-06 19:48:04 +0200

    Added try catch block to main func

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import fs from 'fs/promises';
-import  { resolve } from 'path';
+import { resolve } from 'path';
 import getmostcommonletter from 'getmostcommonletter';
 
 async function getMostCommonLetterFromFile() {
-   const pathStr = process.argv[2];
-   if (!pathStr) {
-      throw new Error('No path were provided')
+   try {
+      const pathStr = process.argv[2];
+      const data = await fs.readFile(resolve(pathStr), 'utf-8');
+      return getmostcommonletter(data);
+   } catch {
+      throw new Error('Wrong path argument');
    }
-   const data = await fs.readFile(resolve(pathStr), 'utf-8');
-   return getmostcommonletter(data);
 }
 
 console.log(await getMostCommonLetterFromFile('lorem.txt'));
